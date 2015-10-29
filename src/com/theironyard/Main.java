ada1bf12f46173fdbb0d1eca37f5f40e3d6fc4de
holdenhughes <holdenmh121489@gmail.com>
2015-10-29 12:16:36 -0400

    Implemented replies

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -34,6 +34,7 @@ public class Main {
                     HashMap m = new HashMap();
                     m.put("threads", threads);
                     m.put("username", username);
+                    m.put("replyId", -1);
                     return new ModelAndView(m, "threads.html");
                 }),
                 new MustacheTemplateEngine()
@@ -42,14 +43,18 @@ public class Main {
         Spark.get(
                 "/replies",
                 ((request, response) ->{
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
+                    m.put("username", username);
 
                     String id = request.queryParams("id");
                     try {
                         int idNum = Integer.valueOf(id);
                         Message message = messages.get(idNum);
                         m.put("message", message);
-
+                        m.put("replyId", message.id);
                         ArrayList<Message> replies = new ArrayList();
                         for (Message msg :messages){
                             if (msg.replyId == message.id) {
@@ -67,33 +72,58 @@ public class Main {
         );
 
         Spark.post(
-                "/login",
+                "/create-message",
                 ((request, response) -> {
-                    String username = request.queryParams("username");
-                    String password = request.queryParams("password");
-
-                    if (username.isEmpty() || password.isEmpty()) {
-                        Spark.halt(403);
-                    }
-
-                    User user = users.get(username);
-                    if (user == null){
-                        user = new User();
-                        user.password = password;
-                        users.put(username, user);
-                    }
-                    else if (!password.equals(user.password)){
-                        Spark.halt(403);
-                    }
-
                     Session session = request.session();
-                    session.attribute("username", username);
+                    String username = session.attribute("username");
 
-                    response.redirect("/");
+                    if (username == null) {
+                        Spark.halt(403);
+                    }
+
+                    String replyId = request.queryParams("replyId");
+                    String text = request.queryParams("text");
+
+                    try {
+                        int replyIdNum = Integer.valueOf(replyId);
+                        Message message = new Message(messages.size(), replyIdNum, username, text);
+                        messages.add(message);
+                    } catch (Exception e) {
+
+                    }
+                    response.redirect(request.headers("Referer"));
                     return "";
                 })
         );
-    }
+
+
+                     Spark.post(
+                             "/login",
+                             ((request, response) -> {
+                                 String username = request.queryParams("username");
+                                 String password = request.queryParams("password");
+
+                                 if (username.isEmpty() || password.isEmpty()) {
+                                     Spark.halt(403);
+                                 }
+
+                                 User user = users.get(username);
+                                 if (user == null) {
+                                     user = new User();
+                                     user.password = password;
+                                     users.put(username, user);
+                                 } else if (!password.equals(user.password)) {
+                                     Spark.halt(403);
+                                 }
+
+                                 Session session = request.session();
+                                 session.attribute("username", username);
+
+                                 response.redirect(request.headers("Referer"));
+                                 return "";
+                             })
+                     );
+                 }
 
     static void addTestUsers(HashMap<String, User> users){
         users.put("Alice", new User());
